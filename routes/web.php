7f7782a713e8203fd074cffb9ffa5013cb6b3bca
sobhan eskandari <serprofessional@gmail.com>
2017-08-14 20:06:55 +0430

    chenged route

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,12 @@
 |
 */
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('Site.home');
 });
 
 Route::get('/home', function () {
